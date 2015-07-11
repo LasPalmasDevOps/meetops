8fbeded928a764c23d8489731a64a7febeaee435
Yeray Gutiérrez Cedrés <ycedres@gmail.com>
2015-07-11 11:00:44 +0100

    First implementation of the Person class

---
+++ src/person.rb
@@ -0,0 +1,24 @@
+class Person
+
+   def initialize(name,age)
+      @name = name
+      @age =  age
+   end
+
+   def name
+     @name
+   end
+
+   def name=(name)
+     @name=name
+   end
+
+   def age
+     @age
+   end
+
+   def age=(age)
+     @age=age
+   end
+
+end
